9926116e25275b96070d2403ad9b5e9883353550
miladlotfi <miladlotfi66@gmail.com>
2023-12-24 19:22:49 +0330

    add price filter logic

--- components/module/searchBar.js
+++ components/module/searchBar.js
@@ -1,27 +1,27 @@
-// import { useState } from "react";
-// import { useRouter } from "next/router";
+import { useState } from "react";
+import { useRouter } from "next/router";
 import styles from "./SearchBar.module.css";
 function SearchBar() {
-  //   const router = useRouter();
+    const router = useRouter();
 
-  //   const [min, setMin] = useState("");
-  //   const [max, setMax] = useState("");
+    const [min, setMin] = useState("");
+    const [max, setMax] = useState("");
 
-  //   const searchHandler = () => {
-  //     if (min && max) {
-  //       router.push(`/filter/${min}/${max}`);
-  //     } else {
-  //       alert("Please Enter minimum and maximum price!");
-  //     }
-  //   };
+    const searchHandler = () => {
+      if (min && max) {
+        router.push(`/filter/${min}/${max}`);
+      } else {
+        alert("Please Enter minimum and maximum price!");
+      }
+    };
 
   return (
     <div className={styles.container}>
       <div>
-        <input placeholder="Inter min-price" />
-        <input placeholder="Inter max-price" />
+        <input placeholder="Inter min-price" value={min} onChange={e=>setMin(e.target.value)}/>
+        <input placeholder="Inter max-price" value={max} onChange={e=>setMax(e.target.value)}/>
       </div>
-      <button>Search</button>
+      <button onClick={searchHandler}>Search</button>
     </div>
   );
 }
